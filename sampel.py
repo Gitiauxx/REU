cb75a772a51bf80b8b4d1e0a060ae4f2ade05a78
Abhispy007 <abhinavkumar98@gmail.com>
2019-06-06 16:42:06 -0400

    histogram

--- sampel.py
+++ sampel.py
@@ -1,4 +1,13 @@
-print("hello world")
-#adassda
-#qdada
-#potti
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+courses = pd.read_csv('C:\\Users\\Pam\\Dropbox\\Data\\FTF\\Fall2016\\nsf_ftf_courses3.csv',low_memory=False)
+degrees = pd.read_csv("C:\\Users\\Pam\\Dropbox\\Data\\FTF\\Fall2016\\nsf_ftf_degrees3.csv",low_memory=False)
+
+x = courses["grdpts"].where(courses['DISC'] == 'MATH')
+x = x.dropna()
+plt.ylabel("Cumulative Math Grades for 2016")
+plt.hist(x)
+plt.show()
