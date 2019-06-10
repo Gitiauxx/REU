4cf8e56cdb05aa332c285b48c7220aa295995e3b
Abhispy007 <abhinavkumar98@gmail.com>
2019-06-10 14:27:20 -0400

    sparse matrix

--- data.py
+++ data.py
@@ -6,17 +6,22 @@ import numpy as np
 courses = pd.read_csv('C:\\Users\\Pam\\Dropbox\\Data\\FTF\\Fall2016\\nsf_ftf_courses3.csv',low_memory=False)
 degrees = pd.read_csv("C:\\Users\\Pam\\Dropbox\\Data\\FTF\\Fall2016\\nsf_ftf_degrees3.csv",low_memory=False)
 
+#demographics_16_15 = pd.read_csv("C:\\Users\\Pam\\Dropbox\\Data\\FTF\\Fall2016\\2016_2015_demographics.csv",low_memory=False)
 
 sample = degrees[(degrees['degmaj1'].isin(['CS']) == True) | (degrees['degmaj1'].isin(['ACS']) == True)]
 #use isin==True to get all CS and ACS degrees
 sample = sample.dropna(axis=1,how='all')
 #drop columns with ALL NA's
-sample = sample.fillna("None")
+
+#sample = sample.fillna("None")
 #fill NA's with None (no major)
 sample = sample.reset_index(drop=True)
 #reset the indicies of the whole dataframe for easier analysis
 sample['id'] = sample['id'].astype(int)
 #cast the id values as int (Python automatically casts them as float for some reason)
+
+sample = sample.fillna(0).to_sparse(fill_value=0)
+#make into sparse matrix, filling in NA's with 0's
 
 '''the above script gets all the CS/ACS majors into 1 dataframe, then cleans up any NaN values'''
 
