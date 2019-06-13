ac9426b8a6d74ce867421ac52fb685248147b8af
gitiauxx <xavier.gitiaux@gmail.com>
2019-06-13 07:14:41 -0400

    use updated data

--- sandbox/student_id.py
+++ sandbox/student_id.py
@@ -13,10 +13,11 @@ student = pd.concat([s1, s2, s3]).drop_duplicates('id').set_index('id')
 print(len(student))
 
 # demographics
+d0 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2015/nsf_demographics12.csv'), low_memory=False)
 d1 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2015/nsf_ftf_demographics2.csv'), low_memory=False)
 d2 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2016/nsf_ftf_demographics3.csv'), low_memory=False)
 d3 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2017/nsf_ftf_demographics4.csv'), low_memory=False)
-demo = pd.concat([d1, d2, d3]).set_index('id')
+demo = pd.concat([d0, d1, d2, d3]).drop_duplicates('id').set_index('id')
 print(len(demo))
 
 # how many can we merge?
