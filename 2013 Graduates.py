35aaaee908ed3e992caf19cd3b0edf74205200d8
Nicci Vyea Casenas <44342843+niccivyea@users.noreply.github.com>
2019-06-20 17:06:48 -0400

    Update 2013 Graduates.py

--- 2013 Graduates.py
+++ 2013 Graduates.py
@@ -10,7 +10,8 @@ admissions2015 = pd.read_csv('REU2015/nsf_demographics12.csv',low_memory=False)
 
 graduated = pd.merge(degrees2015, admissions2015, on = 'id', how = 'left')
 graduated.set_index('id',inplace = True)
-graduated = graduated[((graduated['GRADTERM'].astype(str).str.contains('2013')) == True) & ((graduated.degmaj1.isin(['CS'])==True) | (graduated.degmaj1.isin(['ACS'])==True)) & ((graduated.cohort_pmajr.isin(['CS'])==True) | (graduated.cohort_pmajr.isin(['ACS'])==True))]
+#checks if (cohort is in 2009) & (GRADTERM is in 2013) & (degree graduates is CS or ACS) & (cohort major is CS or ACS)
+graduated = graduated[((graduated['cohort'].astype(str).str.contains('2009')) == True) & ((graduated['GRADTERM'].astype(str).str.contains('2013')) == True) & ((graduated.degmaj1.isin(['CS'])==True) | (graduated.degmaj1.isin(['ACS'])==True)) & ((graduated.cohort_pmajr.isin(['CS'])==True) | (graduated.cohort_pmajr.isin(['ACS'])==True))]
 graduated.to_csv('Graduated2013.csv')
 
 print(graduated)
