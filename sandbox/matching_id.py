35f46f1202981f164ca7d8dd8cf3d47fb08f4e0a
gitiauxx <xavier.gitiaux@gmail.com>
2019-06-21 13:30:42 -0400

    add graduation within 4 years

--- sandbox/matching_id.py
+++ sandbox/matching_id.py
@@ -13,11 +13,12 @@ degrees = pd.concat([degrees0, degrees_2, degrees_1, degrees_3]).drop_duplicates
 
 
 #students
+s0 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2015/nsf_student12.csv'), usecols=['id'], low_memory=False)
 s1 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2015/nsf_ftf_student2.csv'), usecols=['id'], low_memory=False)
 s2 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2016/nsf_ftf_student3.csv'), usecols=['id'], low_memory=False)
 s3 = pd.read_csv(os.path.join(DATA_FILE, 'Fall2017/nsf_ftf_student4.csv'), usecols=['id'], low_memory=False)
 
-student = pd.concat([s1, s2, s3]).drop_duplicates('id').set_index('id')
+student = pd.concat([s0, s1, s2, s3]).drop_duplicates('id').set_index('id')
 
 # degrees-student
 d_s = degrees.join(student, how='inner')
@@ -31,5 +32,6 @@ print(d_s[d_s.degmaj1.isin(['CS', "ACS"])].groupby('cohort_year').size())
 
 # look at students that graduated within 4 years
 d_s['graduation_year'] = d_s.GRADTERM.apply(lambda x: int(str(x)[:4]))
+
 print(d_s[d_s.graduation_year - d_s.cohort_year <= 4].groupby('cohort_year').size())
 print(d_s[(d_s.graduation_year - d_s.cohort_year <= 4) & (d_s.degmaj1.isin(['CS', "ACS"]))].groupby('cohort_year').size())
