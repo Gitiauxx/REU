61b6847074dbb876f0c5b073ac44d3dec2a6b608
Abhi <abhinavkumar98@gmail.com>
2019-06-28 11:19:50 -0400

    -Machine Learning Models for 2009-2012 individuals who started out as CS AND graduated within 4 years

--- Abhi_files/models.py
+++ Abhi_files/models.py
@@ -1,13 +1,22 @@
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
+from sklearn.model_selection import StratifiedKFold
 from sklearn import linear_model
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from scipy.stats import shapiro
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn import linear_model
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import model_selection
+from sklearn.svm import SVC
 from scipy import stats
 from sklearn.linear_model import LogisticRegression
-#from statistics import mean
+from sklearn.preprocessing import KBinsDiscretizer
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,6 +28,18 @@ from sklearn.model_selection import cross_val_score, train_test_split
 
 data = pd.read_csv("/Users/abhi/Documents/REU/Final_Merged.csv")
 
+extra = pd.read_sas("/Users/abhi/Dropbox/nsf_ftf_admissions5.sas7bdat",encoding='latin-1')
+
+data = data[data.cohort != 201310]
+data = data[data.cohort != 201370]
+data = data[data.cohort != 201410]
+data = data[data.cohort != 201470]
+data = data[data.cohort != 201570]
+
+
+#data.cohort = data.cohort.astype(str)
+#data.TERMBNR = data.TERMBNR.astype(str)
+#data.ENTRY_AGE = data.ENTRY_AGE.astype(str)
 zipcode_income = pd.read_csv('/Users/abhi/Documents/MedianZIP-3.csv')
 
 data.rename(columns={'Permanent_Address_ZIP': 'Zip'}, inplace=True)
@@ -36,7 +57,8 @@ new_data.rename(columns={'Median': 'Income'}, inplace=True)
 new_data['Income'] = new_data['Income'].astype(int)
 
 data=new_data.copy(deep=True)
-data.drop(columns=['cohort_pcoll','cohort_pmajr','Income','PMAJR','TERMBNR','cohort','Permanent_Address_STATE','PCOLL',"Application_College"],inplace=True)
+#'cohort','TERMBNR',
+data.drop(columns=['cohort','TERMBNR','cohort_pcoll','cohort_pmajr','Income','PMAJR','Permanent_Address_STATE','PCOLL',"Application_College"],inplace=True)
 
 
 '''
@@ -64,13 +86,14 @@ else:
 exit(2)
 '''
 
-
 X = data[data.columns[:-1]]
 X.drop(columns="id",inplace=True)
 X = pd.get_dummies(X)
+esc = KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='uniform')
+X = esc.fit_transform(X)
 y = data.iloc[:,-1]
 
-
+'''
 random_forest = RandomForestClassifier(n_estimators=1000)
 #logisticCV = linear_model.LogisticRegressionCV(cv=30,max_iter=6000000)
 #decision_tree = svm.SVC(gamma="scale",kernel='rbf',C=.0000001)
@@ -88,18 +111,61 @@ feature_importances = pd.DataFrame(forest.feature_importances_,
                                     columns=['importance']).sort_values('importance',                                                                 ascending=False)
 print(feature_importances.head(10))
 #exit(2)
+'''
+
+import warnings
+warnings.filterwarnings('ignore')  # "error", "ignore", "always", "default", "module" or "once"
+# prepare models
+models = []
+models.append(('Nearest Neighbors', KNeighborsClassifier(5)))
+models.append(('Linear SVM', SVC(kernel='linear', C=0.025)))
+models.append(('Decision Tree',  DecisionTreeClassifier(max_depth=5)))
+models.append(('Random Forest', RandomForestClassifier(n_estimators = 1000, random_state = 42)))
+models.append(('Neural Net', MLPClassifier(alpha=1, max_iter=1000)))
+models.append(('AdaBoost',  AdaBoostClassifier()))
+models.append(('Naive Bayes', GaussianNB()))
+models.append(('SGD Classifier', linear_model.SGDClassifier(max_iter=1000, tol=1e-3)))
+models.append(('LogisticRegressionCV', linear_model.LogisticRegressionCV(cv=5,max_iter=60000)))
+models.append(('K-means',KMeans(n_clusters=2)))
+
+# evaluate each model in turn
+results = []
+names = []
+scoring = 'f1_weighted'
+for name, model in models:
+   kfold = model_selection.StratifiedKFold(n_splits=10, random_state=7)
+   cv_results = model_selection.cross_val_score(model, X, y, cv=5, scoring=scoring)
+   results.append(cv_results)
+   names.append(name)
+   msg = "%s \t%f" % (name, cv_results.mean())
+   print(msg)
+
+# boxplot algorithm comparison
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+#plt.show()
 
 
+'''
 predict = neigh.predict(xTest)
 accuracy = accuracy_score(yTest,predict)
 print("\n")
 print("accuracy: ")
 print(accuracy)
+'''
+
 
 #f1 = f1_score(yTest,predict,average='weighted')
-#results = cross_val_score(logisticCV,X,y,cv=5,scoring='f1_weighted')
-#mean_ = mean(results)
-#print(mean_)
+
+'''
+results = cross_val_score(random_forest,X,y,cv=5,scoring='f1_weighted')
+mean_ = results.mean()
+print(mean_)
+'''
+
 '''
 logisticCV.fit(X,y)
 print(logisticCV.coef_)
